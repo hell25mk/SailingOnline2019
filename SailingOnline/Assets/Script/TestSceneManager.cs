e429e7915470a7ca111943031123e4ab27dca5e3
ny25mk <ny25mk0419@gmail.com>
2019-05-17 10:22:11 +0900

    [add] メニュー画面を追加
    メニュー画面から遷移するように変更

---
+++ SailingOnline/Assets/Script/TestSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestSceneManager : MonoBehaviour {
+
+    private enum eSceneList : int
+    {
+        Scene_Menu = 0,
+        Scene_Online,
+    }
+
+    [SerializeField]
+    private eSceneList moveScene;
+
+    public void SceneMove()
+    {
+        SceneManager.LoadScene((int)moveScene);
+    }
+
+}
