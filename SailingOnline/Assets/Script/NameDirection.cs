455fcab5fca3ba29287ab77f93805012f9d7c324
Tanakai0814 <ta0na8ka1i4yda@gmail.com>
2019-11-22 15:05:15 +0900

    [add]プレイヤー名の同期
    ゲーム画面でプレイヤー名を同期してカメラに向けるようにしました

---
+++ SailingOnline/Assets/Script/NameDirection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameDirection : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        // 回転をカメラと同期させる
+        transform.rotation = Camera.main.transform.rotation;
+    }
+
+}
