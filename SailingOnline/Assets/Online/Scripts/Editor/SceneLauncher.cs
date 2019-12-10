2beaabdb7aa12199f1edff90d638cb7b460e2e5d
ny25mk <ny25mk0419@gmail.com>
2019-12-10 23:43:20 +0900

    [add] エディタ拡張
    Unity内のメニュータブからシーンを切り替えられるように拡張しました

---
+++ SailingOnline/Assets/Online/Scripts/Editor/SceneLauncher.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace Online.Editor
+{
+
+    public class SceneLauncher
+    {
+
+        private const string MenuName = "Online/Scenes/";
+        private const string SceneDirectoryName = "Assets/Online/Scenes/";
+
+        [MenuItem(MenuName + "TitleScene")]
+        public static void OpenTitleScene()
+        {
+
+            EditorSceneManager.OpenScene(SceneDirectoryName + "OnlineTitleScene.unity", OpenSceneMode.Single);
+
+        }
+
+        [MenuItem(MenuName + "MenuScene")]
+        public static void OpenMenuScene()
+        {
+
+            EditorSceneManager.OpenScene(SceneDirectoryName + "Scenes/OnlineMenuScene.unity", OpenSceneMode.Single);
+
+        }
+
+        [MenuItem(MenuName + "MatchingScene")]
+        public static void OpenMatchingScene()
+        {
+
+            EditorSceneManager.OpenScene(SceneDirectoryName + "OnlineMatchingScene.unity", OpenSceneMode.Single);
+
+        }
+
+        [MenuItem(MenuName + "GameScene")]
+        public static void OpenGameScene()
+        {
+
+            EditorSceneManager.OpenScene(SceneDirectoryName + "OnlineGameScene.unity", OpenSceneMode.Single);
+
+        }
+
+        private static void OpenSceneFile()
+        {
+            //最終的に引数をここで受け取ってそれに合わせたファイルを開く予定
+            EditorSceneManager.OpenScene(SceneDirectoryName + "OnlineGameScene.unity", OpenSceneMode.Single);
+
+        }
+
+    }
+
+}
